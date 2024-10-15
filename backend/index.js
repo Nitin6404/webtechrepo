5436700ad776ff320da4ef21d4d4bd9b176cd438
Rohit Maurya <142105480+lusafar1@users.noreply.github.com>
2024-10-15 18:36:01 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// connecting db 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name
+})
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the server'
+    })
+});
+
+app.post('/submit_form', (req, res) => {
+    const { 
+        name, 
+        roll_no, 
+        email, 
+        phone_no, 
+        father_name, 
+        mother_name, 
+        year, 
+        nationality, 
+        gender 
+    } = req.body;
+
+    const sql = 'INSERT INTO form_data (name, roll_no, email, phone_no, father_name, mother_name, year, nationality, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    db.query(sql, [name, roll_no, email, phone_no, father_name, mother_name, year, nationality, gender], (err, result) => {
+        if (err) throw err;
+        res.json({ message: 'Form submitted successfully!' });
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+})
